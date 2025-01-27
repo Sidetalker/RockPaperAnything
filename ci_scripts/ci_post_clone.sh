4e0e469d7c25732d88f49a2179374bd06254b490
Kevin Sullivan <kevin@sideapps.com>
2025-01-27 18:04:13 -0500

    Some extra script logging

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,6 +4,8 @@
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
+echo "Plugin validation disabled"
+
 # Define the source and destination file paths
 source_file="ci_env"
 destination_file="../.env"
@@ -12,7 +14,7 @@ destination_file="../.env"
 if [[ -f "$source_file" ]]; then
     # Copy the source file to the destination
     cp "$source_file" "$destination_file"
-    echo "File copied successfully to $destination_file"
+    echo "$source_file copied successfully to $destination_file"
 else
     echo "Source file $source_file does not exist."
 fi
